18c34bc967bd24cc0ed8f6c4bd0514dd05336f95
gurugeek <gurugeek@mac.com>
2022-03-17 02:26:15 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
   name: "SagaSwimRenderer",
   platforms: [
-    .macOS(.v10_12)
+     .macOS(.v12)
   ],
   products: [
     .library(
